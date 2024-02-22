0af5998236e2a10d34f43d9dbaf722499a475e3b
k.antonenko <akanetka11@gmail.com>
2024-02-22 15:20:35 +0300

    feat: added bottom-bar config

---
+++ src/config/bottom-bar.ts
@@ -0,0 +1,43 @@
+import {
+  HeartActiveIcon,
+  HeartIcon,
+  SaleActiveIcon,
+  SaleIcon,
+  UserIcon,
+} from '../assets';
+import { ImageSourcePropType } from 'react-native';
+import { BottomBarStackRoutes } from '@shared/const/routerBottomBar';
+
+export type BottomBarItem = {
+  label: BottomBarStackRoutes;
+  screenName: string;
+  headerShown: boolean;
+  activeImagePath: ImageSourcePropType;
+  imagePath: ImageSourcePropType;
+};
+
+const bottomBarConfig: { [key in BottomBarStackRoutes]: BottomBarItem } = {
+  [BottomBarStackRoutes.MainStack]: {
+    label: BottomBarStackRoutes.MainStack,
+    screenName: 'Скидки',
+    headerShown: false,
+    activeImagePath: SaleActiveIcon,
+    imagePath: SaleIcon,
+  },
+  [BottomBarStackRoutes.Favorites]: {
+    label: BottomBarStackRoutes.Favorites,
+    screenName: 'Избранное',
+    headerShown: false,
+    activeImagePath: HeartActiveIcon,
+    imagePath: HeartIcon,
+  },
+  [BottomBarStackRoutes.Profile]: {
+    label: BottomBarStackRoutes.Profile,
+    screenName: 'Аккаунт',
+    headerShown: false,
+    activeImagePath: UserIcon,
+    imagePath: UserIcon,
+  },
+};
+
+export default bottomBarConfig;
