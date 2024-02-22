b849902245e16452af44bb281e3371f85153c853
k.antonenko <akanetka11@gmail.com>
2024-02-22 15:15:15 +0300

    feat: added necessary colors in styles folder

---
+++ src/styles/index.ts
@@ -0,0 +1,16 @@
+import { Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+
+export const screenHeight = height;
+export const screenWidth = width;
+
+export const colors = {
+  black: '#19224C',
+  secondary: '#FF245B',
+  primary: '#2F50F4',
+  gray: '#989BB3',
+  natural: '#D1D4E0',
+  lightGray: '#F0F0F5',
+  white: '#FFFFFF',
+};
